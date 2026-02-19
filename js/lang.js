3cb1607392ab0283e2eef3212093fa8842c1793d
Npohsner <npohsner@gmail.com>
2026-02-19 09:41:11 -0300

    Configuração profissional do .gitignore

--- js/lang.js
+++ js/lang.js
@@ -9,10 +9,10 @@ const translations = {
         "menu.schedule": "Programação",
         "menu.howToGet": "Como Chegar",
 
-        "hero.line1": "O início de uma nova era nos trouxe até aqui…",
-        "hero.line2": "Agora, é hora de fazer história!",
-        "hero.date": "29/01/2026 à 01/02/2026",
-        "hero.text": "Muito sol, muita Lambada e Zouk Brasileiro em um Paraíso Tropical! Venha para o MARAÚ DANCE FESTIVAL",
+        "hero.line1": "Em cada giro, um segredo… em cada conexão, um feitiço… a dança conduz aqueles que ousam atravessar para além do que os olhos podem ver.",
+       // "hero.line2": "Agora, é hora de fazer história!",
+        "hero.date": "28/01/2027 à 31/01/2027",
+        "hero.text": "Em um mundo onde o tempo se curva, o sol aquece os sonhos e a lambada encontra o zouk, nasce um paraíso feito para dançar... MARAÚ DANCE FESTIVAL",
 
         "social.title": "Redes Sociais",
         "artists.title": "ARTISTAS",
@@ -29,10 +29,10 @@ const translations = {
         "menu.schedule": "Schedule",
         "menu.howToGet": "How to Get There",
 
-        "hero.line1": "The beginning of a new era has brought us here…",
-        "hero.line2": "Now it’s time to make history!",
-        "hero.date": "Jan 29, 2026 to Feb 1, 2026",
-        "hero.text": "A lot of sun, Lambada and Brazilian Zouk in a Tropical Paradise! Come to the MARAÚ DANCE FESTIVAL",
+        "hero.line1": "In every turn, a secret… in every connection, a spell… the dance guides those who dare to cross beyond what the eyes can see.",
+        //"hero.line2": "Now it’s time to make history!",
+        "hero.date": "Jan 28, 2027 to Jan 31, 2027",
+        "hero.text": "In a world where time bends, the sun warms dreams and lambada meets zouk, a paradise made for dancing is born... MARAÚ DANCE FESTIVAL",
 
         "social.title": "Social Media",
         "artists.title": "ARTISTS",
@@ -50,10 +50,10 @@ const translations = {
         "menu.schedule": "Programación",
         "menu.howToGet": "Cómo llegar",
 
-        "hero.line1": "El inicio de una nueva era nos trajo hasta aquí…",
-        "hero.line2": "¡Ahora es el momento de hacer historia!",
-        "hero.date": "29/01/2026 al 01/02/2026",
-        "hero.text": "Mucho sol, Lambada y Zouk Brasileño en un Paraíso Tropical. ¡Ven al MARAÚ DANCE FESTIVAL!",
+        "hero.line1": "En cada giro, un secreto… en cada conexión, un hechizo… la danza guía a quienes se atreven a cruzar más allá de lo que los ojos pueden ver.",
+        //"hero.line2": "¡Ahora es el momento de hacer historia!",
+        "hero.date": "28/01/2027 al 31/01/2027",
+        "hero.text": "En un mundo donde el tiempo se curva, el sol calienta los sueños y la lambada encuentra al zouk, nace un paraíso hecho para bailar... ¡Ven al MARAÚ DANCE FESTIVAL!",
 
         "social.title": "Redes Sociales",
         "artists.title": "ARTISTAS",
